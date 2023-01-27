8919ed79687e157eff1dc7fc2a7b536e79d1a629
ePoths <epoth@naver.com>
2023-01-28 00:40:55 +0900

    Main

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,19 +3,35 @@ import { useState } from "react";
 
 function Login() {
   const [logInEmail, setLogInEmail] = useState("");
-  const [logInpasword, setLogInPasword] = useState("");
+  const [logInPassword, setLogInPassword] = useState("");
 
-  const login = async () => {};
+  const OnChangeEmail = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setLogInEmail(value);
+  };
+  const OnlogInPassword = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setLogInPassword(value);
+  };
 
   return (
     <div>
       <div>
         <h3>로그인</h3>
-        <input type="email" placeholder="이메일" />
-        <input type="password" placeholder="비밀번호" />
+        <input type="email" placeholder="이메일" onChange={OnChangeEmail} />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          onChange={OnlogInPassword}
+        />
         <button>로그인</button>
       </div>
     </div>
   );
 }
+const login = async () => {};
 export default Login;
--- src/pages/Sigin.tsx
+++ src/pages/Sigin.tsx
@@ -1,22 +1,47 @@
 import * as React from "react";
 import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function Sigin() {
   const [registerEmail, setEegisterEmail] = useState("");
-  const [registerpasword, setRegisterPasword] = useState("");
+  const [registerpassword, setRegisterPassword] = useState("");
 
-  const register = async () => {};
-  const login = async () => {};
-  const logout = async () => {};
+  const OnChangeEmail = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setEegisterEmail(value);
+  };
+  const OnChangePassword = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setEegisterEmail(value);
+  };
+
+  const register = async () => {
+    try{
+  }.catch(error){
+    console.log(error)
+  };
   return (
     <div>
       <div>
         <h3>회원가입</h3>
-        <input type="email" placeholder="이메일" />
-        <input type="password" placeholder="비밀번호" />
-        <button>계정 생성</button>
+        <form action="">
+          <input type="email" placeholder="이메일" onChange={OnChangeEmail} />
+          <input
+            type="password"
+            placeholder="비밀번호"
+            onChange={OnChangePassword}
+          />
+          <button>계정 생성</button>
+        </form>
       </div>
     </div>
   );
 }
 export default Sigin;
+
+const login = async () => {};
+const logout = async () => {};
