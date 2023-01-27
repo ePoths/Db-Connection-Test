597179024488922ce451b3dc3d54c9152ff115a2
ePoths <epoth@naver.com>
2023-01-28 00:58:17 +0900

    Main

--- src/pages/Sigin.tsx
+++ src/pages/Sigin.tsx
@@ -1,34 +1,42 @@
 import * as React from "react";
 import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/config";
 
 function Sigin() {
   const [registerEmail, setEegisterEmail] = useState("");
   const [registerpassword, setRegisterPassword] = useState("");
-
+  //
   const OnChangeEmail = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = event;
     setEegisterEmail(value);
   };
+
   const OnChangePassword = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = event;
-    setEegisterEmail(value);
+    setRegisterPassword(value);
   };
 
-  const register = async () => {
-    try{
-  }.catch(error){
-    console.log(error)
+  //
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    createUserWithEmailAndPassword(auth, registerEmail, registerpassword)
+      .then(() => {
+        alert("회원 가입 성공");
+      })
+      .catch((e) => {
+        alert(e);
+      });
   };
   return (
     <div>
       <div>
         <h3>회원가입</h3>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <input type="email" placeholder="이메일" onChange={OnChangeEmail} />
           <input
             type="password"
@@ -42,6 +50,3 @@ function Sigin() {
   );
 }
 export default Sigin;
-
-const login = async () => {};
-const logout = async () => {};
